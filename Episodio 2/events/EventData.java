88d421eecfc98cb1f9f47f855201e685787a2763
Danidev819 <38832869+Danidev819@users.noreply.github.com>
2022-12-26 16:03:14 +0100

    Create EventData.java

---
+++ Episodio 2/events/EventData.java
@@ -0,0 +1,16 @@
+package me.danipro.tutorialclient.client.events;
+
+import java.lang.reflect.Method;
+
+public class EventData {
+
+    public final Object source;
+    public final Method target;
+    public final EventPriority priority;
+
+    public EventData(Object source, Method target, EventPriority priority) {
+        this.source = source;
+        this.target = target;
+        this.priority = priority;
+    }
+}
